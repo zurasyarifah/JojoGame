48c4577c74950c4b43e73f8f9d9043d94fe6011b
zurasyarifah <zurasyarifah@gmail.com>
2017-06-16 13:49:26 +0700

    masukin image benar dan salah ke folder img

--- js/app.js
+++ js/app.js
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-var w_width=window.innerwidht; //baca panjang layar
-$("#story").css("width",w_width/2);
-=======
-var w_width=window.innerWidth
-
+var w_width=window.innerWidth //baca panjang layar
 $('#story').css('width',(w_width/2.2));
->>>>>>> 3cace096d31e3c1c10693569b654c93b10316441
